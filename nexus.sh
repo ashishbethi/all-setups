3fe5703e52cdbd01212f13941ae1d9a33d4a4e3e
ashishbethi <40888835+ashishbethi@users.noreply.github.com>
2026-03-13 12:32:45 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -4,6 +4,6 @@ wget https://download.sonatype.com/nexus/3/nexus-3.90.1-01-linux-x86_64.tar.gz
 tar -zxvf nexus-3.90.1-01-linux-x86_64.tar.gz
 useradd nexus
 chown -R nexus:nexus nexus-3.90.1-01 sonatype-work
-su – nexus
+su - nexus
 cd /opt /nexus-3.90.21-01-linux/bin/
 ./nexus start
